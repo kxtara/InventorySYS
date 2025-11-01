977a3e0c5ab69c9c36cca507eedbb3ac9da346d5
kiara hoheb <Kiara Hoheb>
2025-11-01 17:31:00 -0400

    Formatting

--- db.h
+++ db.h
@@ -6,6 +6,8 @@
 PGconn* connectToDB(const std::string& conninfo);
 void runQuery(PGconn* conn, const std::string& query);
 void closeConnection(PGconn* conn);
+
+// Customer
 void viewCustomers(PGconn* conn);
 void addCustomer(PGconn* conn, const std::string& name, const std::string& address, const std::string& phone);
 void deleteCustomer(PGconn* conn, int customerId);
