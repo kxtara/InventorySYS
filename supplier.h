2bbaca47dabff8f26c9fae97d6db8c7be4214356
Kiara Hoheb <kiarahoheb@gmail.com>
2025-11-04 17:41:07 -0400

    Declare update/delete functions

--- supplier.h
+++ supplier.h
@@ -4,4 +4,10 @@
 
 void viewSuppliers(PGconn* conn);
 void addSupplier(PGconn* conn, const std::string& suppliername, const std::string& phone, const std::string& email);
+void viewSupplier(PGconn* conn, int supplierId);
+
+// update product - take in column values to make changes
+void updateSupplier(PGconn* conn, int supplierId);
+
+void deleteSupplier(PGconn* conn, int supplierId);
 // view by specfic supplier, update supplier info, delete product
